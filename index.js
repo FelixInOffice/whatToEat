95716361cf02b0f7fab09b4f1b7f16e550696da2
Felix <86642452+FelixInOffice@users.noreply.github.com>
2022-06-29 18:54:11 +0800

    Add files via upload

---
+++ index.js
@@ -0,0 +1,14 @@
+if ('serviceWorker' in navigator) {
+    navigator.serviceWorker.register('./sw.js').then(reg => {
+        reg.onupdatefound = () => {
+            const installingWorker = reg.installing;
+            installingWorker.onstatechange = () => {
+                if (installingWorker.state === 'installed' &&
+                    navigator.serviceWorker.controller) {
+                    // Preferably, display a message asking the user to reload...
+                    showUpdate();
+                }
+            };
+        };
+    });
+}
---
+++ sw.js
@@ -0,0 +1,57 @@
+// Cache name
+const cacheName = 'v1';
+
+// install cache
+self.addEventListener('install', e => {
+    e.waitUntil(
+        caches.open(cacheName).then(cache => {
+            return cache.addAll(["./",
+                "./index.js",
+                "./sw.js",
+                "./manifest.json",
+                "./assets/scripts/script.js",
+                "./assets/styles/style.css",
+                "./assets/images/maskable_icon.png",
+                "./assets/images/icon.png",
+                "./assets/images/icon-72x72.png",
+                "./assets/images/icon-96x96.png",
+                "./assets/images/icon-128x128.png",
+                "./assets/images/icon-144x144.png",
+                "./assets/images/icon-152x152.png",
+                "./assets/images/icon-192x192.png",
+                "./assets/images/icon-384x384.png",
+                "./assets/images/icon-512x512.png",
+            ]) + skipWaiting();
+
+        })
+    );
+});
+
+// request cache
+self.addEventListener('fetch', function (event) {
+    event.respondWith(
+        caches.match(event.request).then(function (resp) {
+            return resp || fetch(event.request).then(function (response) {
+                return caches.open(cacheName).then(function (cache) {
+                    cache.put(event.request, response.clone());
+                    return response;
+                });
+            });
+        })
+    );
+});
+
+// update and delete old cache
+self.addEventListener('activate', (event) => {
+    var cacheKeeplist = [cacheName];
+
+    event.waitUntil(
+        caches.keys().then((keyList) => {
+            return Promise.all(keyList.map((key) => {
+                if (cacheKeeplist.indexOf(key) === -1) {
+                    return caches.delete(key);
+                }
+            }));
+        })
+    );
+});
